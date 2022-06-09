f232040b94be859387315173401ad97bc80d7d18
michael clark <michaelclark@michaels-MacBook-Air.local>
2022-06-09 14:09:19 -0700

    added navigation tab dependency and boilerplate for additonal screens

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { createStackNavigator } from 'react-navigation-stack'
 import HomeScreen from './src/screens/homeScreen'
 import loginScreen from './src/screens/loginScreen';
 import MapScreen from './src/screens/mapScreen';
+import AccountScreen from './src/screens/accountScreen';
+import GamesScreen from './src/screens/gamesScreen';
+import SignupScreen from './src/screens/signupScreen';
 
 const navigator = createStackNavigator({
   Home: HomeScreen,
---
+++ src/screens/gamesScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const GamesScreen = () => {
+    return <Text style={{ fontSize: 30 }}> GamesScreen</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default GamesScreen;
+ 
