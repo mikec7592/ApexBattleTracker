50ba16a223fca07f16364e15eba6fb33d882f1b3
michael clark <michaelclark@michaels-MacBook-Air.local>
2022-06-10 14:44:46 -0700

    Extracted reusable code from log in form into a reusable component.  Cleaned up imports no longer in use on signupScreen

--- src/screens/signupScreen.js
+++ src/screens/signupScreen.js
@@ -1,40 +1,20 @@
-import React, { useState, useContext } from 'react';
+import React, {  useContext } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
+import { Text } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
 
 const SignupScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     return (
     <View style={styles.container}>
-        <Spacer>
-            <Text h3> Become A Tracker</Text>
-        </Spacer>
-        <Input 
-        label='Email'
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize='none'
-        autoCorrect={false} 
+        <AuthForm 
+            headerText='Become A Tracker'
+            errorMessage={state.errorMessage}
+            buttonText='Sign Up'
+            onSubmit={signup}
         />
-        <Spacer />
-        <Input
-        secureTextEntry 
-        label='Password' 
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize='none'
-        autoCorrect={false}
-        />
-        {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
-        <Spacer>
-            <Button 
-            title='Sign Up'
-            onPress={() => signup({ email, password })} />
-        </Spacer>
         <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
             <Spacer>
                 <Text style={styles.link} >Already have an account?  Go to sign in.</Text>
@@ -57,12 +37,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 160
-    },
-    errorMessage: {
-        fontSize: 15,
-        color: 'red',
-        marginLeft: 16,
-        marginTop: 10
     },
     link: {
         color: 'blue'
