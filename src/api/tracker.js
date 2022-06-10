d63ad97fccfb170187ce77530385a8cd7d70e9d6
michael clark <michaelclark@michaels-MacBook-Air.local>
2022-06-10 13:01:47 -0700

    added axios for network requests.  Added call back function on button press  to send signup request

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: ' http://3778-2601-600-807f-e240-4052-8878-78c9-2ae6.ngrok.io'
+});
--- src/screens/signupScreen.js
+++ src/screens/signupScreen.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+    const { state, signup } = useContext(AuthContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     return (
@@ -28,7 +30,9 @@ const SignupScreen = ({ navigation }) => {
         autoCorrect={false}
         />
         <Spacer>
-            <Button title='Sign Up' />
+            <Button 
+            title='Sign Up'
+            onPress={() => signup({ email, password })} />
         </Spacer>
     </View>
     );
