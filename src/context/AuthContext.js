e2c0eafe4a900a299d240dc3d86ef86a0246e142
michael clark <michaelclark@michaels-MacBook-Air.local>
2022-06-17 14:34:51 -0700

    added function to remove JWT from lacal storage in order to signout from account screen.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,6 +11,8 @@ const authReducer = (state, action) => {
             return{ errorMessage: '', token: action.payload };
         case 'clear_error_message':
             return { ...state, errorMessage: ''};
+        case 'signout': 
+            return { token: null, errorMessage: '' }
         default:
             return state;
     }
@@ -69,11 +71,11 @@ const signup = dispatch => async ({ email, password }) => {
 //         }
 //     };
 
-const signout = (dispatch) => {
-    return () => {
-        // signout
-    }
-}
+const signout = dispatch => async () => {
+    await AsyncStorage.removeItem('token');
+    dispatch({ type: 'signout' });
+    navigate('loginFlow')
+};
 
 export const { Provider, Context } = createDataContext(
     authReducer,
--- src/screens/accountScreen.js
+++ src/screens/accountScreen.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountScreen = () => {
-    return <Text style={{ fontSize: 30 }}> AccountScreen</Text>
+    const { signout } = useContext(AuthContext)
+    return <>
+        <Text style={{ fontSize: 30 }}> AccountScreen</Text>
+        <Spacer />
+        <Button 
+        title='Sign Out'
+        onPress={signout}
+        />
+        <Spacer />
+     </>
 };
 
 const styles = StyleSheet.create({});
