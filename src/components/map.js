a77559f50467b34d2ddf4e4801a97c8128959742
michael clark <michaelclark@michaels-MacBook-Air.local>
2022-06-21 16:24:58 -0700

    added safe zone on create screen to pull text from top of screen.  Boiler place for a seperate map compnonent that can be used when creating a game.

---
+++ src/components/map.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet, Text, Image } from 'react-native'
+
+const Map = () => {
+    return (
+        <Text>Map Here</Text>
+    )
+};
+
+const Styles = StyleSheet.create({});
+
+export default Map;
--- src/screens/gameCreateScreen.js
+++ src/screens/gameCreateScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Map from '../components/map';
 
 const GameCreateScreen = () => {
-    return <Text style={{ fontSize: 30 }}> GameCreateScreen</Text>
+    return (
+        <SafeAreaView forceInset={{ top: 'always' }}> 
+            <Text h2> Log a Game </Text>
+            <Map />
+        </SafeAreaView>
+    )
 };
 
 const styles = StyleSheet.create({});
