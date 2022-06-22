5ca549a5a653564c7ae1f0186d869f9955af4c27
michael clark <michaelclark@michaels-MacBook-Air.local>
2022-06-22 16:25:12 -0700

    Boilerplate for game form component that can be used when creating and editing stats from a game

---
+++ src/components/GameForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Input, Button } from "react-native-elements";
+import Spacer from "./Spacer";
+
+const GameForm = () => {
+    return (
+        <>
+            <Input placeholder="Damage" />
+        </>
+    );
+};
+
+export default GameForm;
--- src/screens/gameCreateScreen.js
+++ src/screens/gameCreateScreen.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import GameForm from '../components/GameForm';
 import Map from '../components/map';
+import Spacer from '../components/Spacer';
 
 const GameCreateScreen = () => {
     return (
         <SafeAreaView forceInset={{ top: 'always' }}> 
             <Text h2> Log a Game </Text>
             <Map />
+            <Spacer />
+                <GameForm />
+            <Spacer />
         </SafeAreaView>
-    )
+    );
 };
 
 const styles = StyleSheet.create({});
